0639bf3366cc8f04fb53f7bcc3de9dea03f86241
Travis Bernard <travis.bernard@shipperhq.com>
2021-10-07 19:08:10 -0500

    adds rollup and sets up TS config

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from 'rollup-plugin-typescript'
+
+export default {
+   input: 'src/index.tsx',
+   output: {
+      dir: 'dist',
+      format: 'iife',
+   },
+   plugins: [
+      typescript()
+   ]
+}
